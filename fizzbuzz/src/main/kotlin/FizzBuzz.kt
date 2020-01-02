b493d33b74f3ccf501f58649e9b54a2e366f06e4
RedColinar <1126525692@qq.com>
2020-01-02 23:04:02 +0800

    use junit 5

--- fizzbuzz/src/main/kotlin/FizzBuzz.kt
+++ fizzbuzz/src/main/kotlin/FizzBuzz.kt
@@ -1,6 +1,6 @@
-class Gamer {
+class FizzBuzz {
     companion object {
-        fun toString(value: Int): String {
+        fun of(value: Int): String {
             if (isRelateTo(value, 3) && isRelateTo(value, 5)) return "fizzbuzz"
             if (isRelateTo(value, 3)) return "fizz"
             if (isRelateTo(value, 5)) return "buzz"
@@ -11,11 +11,5 @@ class Gamer {
             if (value % relate == 0 || value.toString().contains(relate.toString())) return true
             return false
         }
-    }
-}
-
-fun main() {
-    for (i in 1..100) {
-        println(Gamer.toString(i))
     }
 }
--- fizzbuzz/src/test/kotlin/MainTest.kt
+++ fizzbuzz/src/test/kotlin/MainTest.kt
@@ -1,33 +1,17 @@
-import junit.framework.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class MainTest {
-    @Test
-    fun test_raw_number() {
-        assertEquals("1", Gamer.toString(1))
-    }
 
-    @Test
-    fun test_3_number() {
-        assertEquals("fizz", Gamer.toString(3))
-    }
-
-    @Test
-    fun test_5_number() {
-        assertEquals("buzz", Gamer.toString(5))
-    }
-
-    @Test
-    fun test_3_5() {
-        assertEquals("fizzbuzz", Gamer.toString(15))
-    }
-
-    @Test
-    fun testEquals() {
-        val main = Main()
-        for (i in 1..300) {
-            assertEquals("问题在 $i", main.match(i), Gamer.toString(i))
-        }
+    @ParameterizedTest(name = "input {0} should return {1}")
+    @CsvSource(
+        "1, '1'",
+        "3, 'fizz'",
+        "5, 'buzz'"
+    )
+    fun test(input: Int, word: String) {
+        assertEquals(word, FizzBuzz.of(input))
     }
 }
 
